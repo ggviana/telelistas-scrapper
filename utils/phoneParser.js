3f20559ed988ce83fd7f984f339778b47057b2d9
Guilherme Viana <ggviana@hotmail.com.br>
2016-08-17 03:24:23 -0300

    Better namings and export at the top

--- utils/phoneParser.js
+++ utils/phoneParser.js
@@ -1,20 +1,3 @@
-const digit1map = {
-  "6F": 0, "6E": 1, "6D": 2, "6C": 3, "6B": 4,
-  "6A": 5, "69": 6, "68": 7, "67": 8, "66": 9,
-}
-
-const digit2map = {
-  "7D": 0, "7C": 1, "7F": 2, "7E": 3, "79": 4, 
-  "78": 5, "7B": 6, "7A": 7, "75": 8, "74": 9,
-}
-
-const decodeDigits = text => {
-  const firstDigit  = text.substring(0,2)
-  const secondDigit = text.substring(2,4)
-
-  return `${digit1map[firstDigit]}${digit2map[secondDigit]}`
-}
-
 module.exports.parse = (content, encodedDigits) => {
   const phones = content.trim()
 
@@ -28,4 +11,21 @@ module.exports.parse = (content, encodedDigits) => {
     .map(phone => phone.trim())
     .map((phone, index) => `${phone}${lastDigits[index]}`)
     .join(', ')
+}
+
+const FIRST_DIGIT_MAP = {
+  "6F": 0, "6E": 1, "6D": 2, "6C": 3, "6B": 4,
+  "6A": 5, "69": 6, "68": 7, "67": 8, "66": 9,
+}
+
+const SECOND_DIGIT_MAP = {
+  "7D": 0, "7C": 1, "7F": 2, "7E": 3, "79": 4,
+  "78": 5, "7B": 6, "7A": 7, "75": 8, "74": 9,
+}
+
+const decodeDigits = text => {
+  const firstDigit  = text.substring(0,2)
+  const secondDigit = text.substring(2,4)
+
+  return `${FIRST_DIGIT_MAP[firstDigit]}${SECOND_DIGIT_MAP[secondDigit]}`
 }
